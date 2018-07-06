b1d526e1acc7dd969811d1424f0b8f4b7b70d718
Yaakov <yaakov.chaikin@gmail.com>
2018-07-06 19:10:30 -0400

    WIP. Tests in demo and framework for GET request.

--- src/test/java/com/clearlydecoded/messenger/demo/service/ApplicationConfig.java
+++ src/test/java/com/clearlydecoded/messenger/demo/service/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package com.clearlydecoded.messenger.demo.service;
 
+import com.clearlydecoded.messenger.rest.SpringRestMessenger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 public class ApplicationConfig {
+
+  @Bean
+  public SpringRestMessenger createSpringRestCommander(ApplicationContext springContext) {
+    return new SpringRestMessenger(springContext);
+  }
 
 }
--- src/test/java/com/clearlydecoded/messenger/demo/service/MessageRestEndpointTest.java
+++ src/test/java/com/clearlydecoded/messenger/demo/service/MessageRestEndpointTest.java
@@ -3,12 +3,18 @@ package com.clearlydecoded.messenger.demo.service;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ApplicationConfig.class)
-//@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationConfig.class)
+@AutoConfigureMockMvc
 public class MessageRestEndpointTest {
 
   @Autowired
@@ -22,5 +28,14 @@ public class MessageRestEndpointTest {
 //    //        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 //    //        .andExpect(content().string(expectedResponseString));
 //  }
+
+  @Test
+  public void testGetAvailableCommands() throws Exception {
+    mvc.perform(get("/process")).andDo(print());
+    //        .contentType(MediaType.APPLICATION_JSON))
+    //        .andExpect(status().isOk())
+//            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    //        .andExpect(content().string(expectedResponseString));
+  }
 
 }
